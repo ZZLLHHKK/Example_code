d3d478586de54ea96d76bf76b701ba30cb921480
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-05-14 22:00:44 +0800

    Create setter & getter and constructor & destructor.cpp

---
+++ 112-2/setter & getter and constructor & destructor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class A{
+private:
+    int x;
+public:
+    A() {
+        cout << "object A construct." << endl;
+    }
+    A(int _x) : x(_x) {}
+    ~A() {
+        cout << "object A destroyed." << endl;
+    }
+    void setX(int _x) {
+        x = _x;
+    }
+    int getX() {
+        return x;
+    }
+    void display() {
+        cout << "object A has a value integer " << getX() << endl;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+
+    A a;
+    a.setX(t);
+    a.display();
+}
